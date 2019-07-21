49c8d7535000c56646d5559a047ad8b9d05e47c9
LakshmiRadhakrishnan <lakshmi.radhakrishnan@ellucian.com>
2019-07-21 19:12:38 -0400

    9.32 from master; misc jira fixes; add interceptors to replace filters; enable codenarc at app levelintroduce proxy mgmt interceptor

---
+++ grails-app/controllers/GeneralSelfServicePersonalInformationDetailsInterceptor.groovy
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ Copyright 2019 Ellucian Company L.P. and its affiliates.
+ ****************************************************************************** */
+
+import net.hedtech.banner.general.GeneralSsbConfigService
+
+class GeneralSelfServicePersonalInformationDetailsInterceptor {
+    def generalSsbConfigService
+
+    GeneralSelfServicePersonalInformationDetailsInterceptor() {
+        match (controller: 'personalInformationDetails').excludes(action: 'getPersonalDetails')
+                                                        .excludes(action:'getBannerId')
+                                                        .excludes(action:'getUserName')
+                                                        .excludes(action:'getPiConfig')
+
+    }
+    boolean before()
+    {
+        if (generalSsbConfigService.getParamFromSession(GeneralSsbConfigService.ENABLE_PERSONAL_INFORMATION, 'Y') != 'Y') {
+            redirect(controller: "error", action: "accessForbidden")
+            return false
+        }
+        true
+
+    }
+
+    boolean after() { true }
+
+    void afterView() {
+        // no-op
+    }
+}
